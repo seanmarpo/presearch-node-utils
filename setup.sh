3cb6cf04aad999d998af27405ea25bdbe76fec9f
Sean Marpo <marpo.sean@gmail.com>
2021-06-27 12:22:57 -0700

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+if ! command -v docker &> /dev/null
+then
+    echo "Docker was not found, installing Docker..."
+    curl https://get.docker.com/ | sudo sh
+    exit
+else
+    echo "Docker appears available, attempting to start node."
+    read -sp "Presearch Node API Key: " PREAPIKEY
+    sudo docker stop presearch-node
+    sudo docker rm presearch-node
+    sudo docker stop presearch-auto-updater
+    sudo docker rm presearch-auto-updater
+    sudo docker run -d --name presearch-auto-updater --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --interval 300 presearch-node
+    sudo docker pull presearch/node
+    sudo docker run -dt --name presearch-node --restart=unless-stopped -v presearch-node-storage:/app/node -e REGISTRATION_CODE=$PREAPIKEY presearch/node
+    sudo docker logs presearch-node
+fi
